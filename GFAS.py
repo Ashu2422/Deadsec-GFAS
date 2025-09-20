95f81aa77e6599b9c07dd554f3493006904d3a4c
Ashu2422 <145796489+Ashu2422@users.noreply.github.com>
2025-09-20 14:04:26 +0530

    GFAS.py

--- GFAS.py
+++ GFAS.py
@@ -6,7 +6,8 @@ from typing import Dict, List, Tuple
 import numpy as np
 import pandas as pd
 import pydeck as pdk
-import streamlit as st
+import streamlit import st_folium
+
 import folium
 from streamlit_folium 
 from PIL import Image
